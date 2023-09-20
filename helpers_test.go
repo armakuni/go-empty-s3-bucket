0ab5157ea3071cd82e1a7357f517c447a4c5bd40
Tom Oram <tom.oram@armakuni.com>
2023-09-20 16:06:34 +0100

    refactor: Create SimpleS3Client

--- helpers_test.go
+++ helpers_test.go
@@ -2,30 +2,80 @@ package emptys3bucket_test
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/stretchr/testify/require"
+	"strings"
 	"testing"
 )
 
-func createBucket(s3Client *s3.Client, bucketName string) (func() error, error) {
-	_, err := s3Client.CreateBucket(context.TODO(), &s3.CreateBucketInput{
+type SimpleS3Client struct {
+	S3Client *s3.Client
+	region   string
+}
+
+func NewTestS3Client(awsRegion string) (*SimpleS3Client, error) {
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(awsRegion))
+	if err != nil {
+		return nil, errors.New("could not setup config")
+	}
+	s3Client := s3.NewFromConfig(cfg)
+
+	return &SimpleS3Client{
+		S3Client: s3Client,
+		region:   awsRegion,
+	}, nil
+}
+
+func (client SimpleS3Client) CreateBucket(bucketName string) error {
+	_, err := client.S3Client.CreateBucket(context.TODO(), &s3.CreateBucketInput{
 		Bucket: aws.String(bucketName),
 		CreateBucketConfiguration: &types.CreateBucketConfiguration{
-			LocationConstraint: types.BucketLocationConstraint(awsRegion),
+			LocationConstraint: types.BucketLocationConstraint(client.region),
 		},
 	})
 	if err != nil {
-		return nil, fmt.Errorf("failed to create bucket %s: %s", bucketName, err)
+		return fmt.Errorf("failed to create bucket %s: %s", bucketName, err)
 	}
-
-	return func() error { return deleteBucket(s3Client, bucketName) }, nil
+	return nil
 }
 
-func deleteBucket(s3Client *s3.Client, bucketName string) error {
-	_, err := s3Client.DeleteBucket(context.TODO(), &s3.DeleteBucketInput{
+func (client SimpleS3Client) CreateVersionedBucket(bucketName string) error {
+	err := client.CreateBucket(bucketName)
+	if err != nil {
+		return err
+	}
+
+	_, err = client.S3Client.PutBucketVersioning(context.TODO(), &s3.PutBucketVersioningInput{
+		Bucket: aws.String(testBucketName),
+		VersioningConfiguration: &types.VersioningConfiguration{
+			Status: types.BucketVersioningStatusEnabled,
+		},
+	})
+
+	if err != nil {
+		return fmt.Errorf("could not apply versioning on bucket %s: %s", bucketName, err)
+	}
+
+	return err
+}
+
+func (client SimpleS3Client) PutObject(bucketName string, key string, content string) error {
+	_, err := client.S3Client.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(key),
+		Body:   strings.NewReader(content),
+	})
+
+	return err
+}
+
+func (client SimpleS3Client) DeleteBucket(bucketName string) error {
+	_, err := client.S3Client.DeleteBucket(context.TODO(), &s3.DeleteBucketInput{
 		Bucket: aws.String(bucketName),
 	})
 
--- main_test.go
+++ main_test.go
@@ -1,149 +1,61 @@
 package emptys3bucket_test
 
 import (
-	"context"
-	"errors"
-	"fmt"
-	"os"
-	"strings"
 	"testing"
 
 	emptys3bucket "github.com/armakuni/go-empty-s3-bucket"
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/stretchr/testify/require"
 )
 
-// tests needed:
-// - empty bucket with no versioning
-// - empty bucket with versioning
-// - empty bucket with versioning and delete markers
-// - empty bucket with versioning and delete markers and delete all versions
 const awsRegion = "eu-west-1"
-const unversionedBucketName = "emptys3bucket-unversioned-integration-test"
-const versionedBucketName = "emptys3bucket-versioned-integration-test"
-
-var s3Client *s3.Client
-
-func setup() ([]func() error, error) {
-	var cleanupFunctions []func() error
-
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(awsRegion))
-	if err != nil {
-		return cleanupFunctions, errors.New("could not setup config")
-	}
-	s3Client = s3.NewFromConfig(cfg)
-
-	bucketsToCreate := []string{unversionedBucketName, versionedBucketName}
-
-	for _, bucketName := range bucketsToCreate {
-		deleteBucketFunction, err := createBucket(s3Client, bucketName)
-		if deleteBucketFunction != nil {
-			cleanupFunctions = append(cleanupFunctions, func() error { return deleteBucketFunction() })
-		}
-		if err != nil {
-			return cleanupFunctions, err
-		}
-	}
-
-	_, err = s3Client.PutBucketVersioning(context.TODO(), &s3.PutBucketVersioningInput{
-		Bucket: aws.String(unversionedBucketName),
-		VersioningConfiguration: &types.VersioningConfiguration{
-			Status: types.BucketVersioningStatusEnabled,
-		},
-	})
-	if err != nil {
-		return cleanupFunctions, errors.New("could not apply versioning on bucket")
-	}
-
-	return cleanupFunctions, nil
-}
-
-func cleanup(cleanupFunctions []func() error) []error {
-	var errs []error
-
-	for _, function := range cleanupFunctions {
-		err := function()
-		if err != nil {
-			errs = append(errs, err)
-		}
-	}
-
-	return errs
-}
-
-func TestMain(m *testing.M) {
-	var exitCode = 0
-
-	cleanupFunctions, err := setup()
-	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "An error occurred in setup(): "+err.Error())
-		exitCode = 1
-	} else {
-		exitCode = m.Run()
-	}
-
-	errs := cleanup(cleanupFunctions)
-
-	if errs != nil {
-		for _, err = range errs {
-			_, _ = fmt.Fprintf(os.Stderr, "An error occurred in cleanup(): "+err.Error())
-		}
-		if exitCode == 0 {
-			exitCode = 1
-		}
-	}
-
-	os.Exit(exitCode)
-}
+const testBucketName = "emptys3bucket-integration-test"
 
 func TestEmptyBucketWithOneItemAndNoVersioning(t *testing.T) {
-	_, err := s3Client.PutObject(context.TODO(), &s3.PutObjectInput{
-		Bucket: aws.String(unversionedBucketName),
-		Key:    aws.String("index.html"),
-		Body:   strings.NewReader("<h1>Hello World</h1>"),
-	})
+	simpleS3Client, err := NewTestS3Client(awsRegion)
 	require.Nil(t, err)
 
-	emptys3bucket.EmptyBucket(s3Client, unversionedBucketName)
+	err = simpleS3Client.CreateBucket(testBucketName)
+	require.Nil(t, err)
+	defer simpleS3Client.DeleteBucket(testBucketName)
 
-	assertBucketIsEmpty(t, s3Client, unversionedBucketName)
+	err = simpleS3Client.PutObject(testBucketName, "index.html", "<h1>Hello World</h1>")
+	require.Nil(t, err)
+
+	emptys3bucket.EmptyBucket(simpleS3Client.S3Client, testBucketName)
+
+	assertBucketIsEmpty(t, simpleS3Client.S3Client, testBucketName)
 }
 
 func TestEmptyBucketWithTwoItemsAndNoVersioning(t *testing.T) {
-	files := []string{"index.html", "index2.html"}
-	for _, file := range files {
-		_, err := s3Client.PutObject(context.TODO(), &s3.PutObjectInput{
-			Bucket: aws.String(unversionedBucketName),
-			Key:    aws.String(file),
-			Body:   strings.NewReader("<h1>Hello World</h1>"),
-		})
-		require.Nil(t, err)
-	}
+	simpleS3Client, err := NewTestS3Client(awsRegion)
+	require.Nil(t, err)
 
-	emptys3bucket.EmptyBucket(s3Client, unversionedBucketName)
+	err = simpleS3Client.CreateBucket(testBucketName)
+	require.Nil(t, err)
+	defer simpleS3Client.DeleteBucket(testBucketName)
 
-	assertBucketIsEmpty(t, s3Client, unversionedBucketName)
+	err = simpleS3Client.PutObject(testBucketName, "index.html", "<h1>Hello World</h1>")
+	err = simpleS3Client.PutObject(testBucketName, "index2.html", "<h1>Hello World</h1>")
+	require.Nil(t, err)
+
+	emptys3bucket.EmptyBucket(simpleS3Client.S3Client, testBucketName)
+
+	assertBucketIsEmpty(t, simpleS3Client.S3Client, testBucketName)
 }
 
 func TestEmptyBucketWithVersioningEnabled(t *testing.T) {
-	_, err := s3Client.PutObject(context.TODO(), &s3.PutObjectInput{
-		Bucket: aws.String(versionedBucketName),
-		Key:    aws.String("index.html"),
-		Body:   strings.NewReader("<h1>Version 1</h1>"),
-	})
+	simpleS3Client, err := NewTestS3Client(awsRegion)
 	require.Nil(t, err)
 
-	_, err = s3Client.PutObject(context.TODO(), &s3.PutObjectInput{
-		Bucket: aws.String(versionedBucketName),
-		Key:    aws.String("index.html"),
-		Body:   strings.NewReader("<h1>Version 2</h1>"),
-	})
+	err = simpleS3Client.CreateVersionedBucket(testBucketName)
+	require.Nil(t, err)
+	defer simpleS3Client.DeleteBucket(testBucketName)
+
+	err = simpleS3Client.PutObject(testBucketName, "index.html", "<h1>Version 1</h1>")
+	err = simpleS3Client.PutObject(testBucketName, "index.html", "<h1>Version 2</h1>")
 	require.Nil(t, err)
 
-	emptys3bucket.EmptyBucket(s3Client, versionedBucketName)
+	emptys3bucket.EmptyBucket(simpleS3Client.S3Client, testBucketName)
 
-	assertBucketIsEmpty(t, s3Client, versionedBucketName)
+	assertBucketIsEmpty(t, simpleS3Client.S3Client, testBucketName)
 }
